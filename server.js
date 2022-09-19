4bc644c2fa6a3a80e3f3a71445257f31030d4144
Beast1610 <singhharsan@gmail.com>
2022-09-19 18:21:28 +0530

    creating routes in server and methods

--- server.js
+++ server.js
@@ -3,13 +3,49 @@ const { listenerCount } = require('process');
 
 const port = 8081; //local port number
 
+const toDolist = ["Complete Node Byte", "Play Cricket"];
+
 http
-    .createServer((request, response) => {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<h1>Hello, this is from my server</h1>");
-        response.end();
+    .createServer((req,res) => 
+    {
+        const { method,url } = req;
+
+        if( url === "/todos"){
+            if ( method === "GET"){
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(toDolist.toString());
+            } else if ( method === "POST"){
+                let body = "";
+                req.on('error',(err) => {
+                    console.error(err)                  
+                })
+                .on('data',(chunk) => {
+                    body += chunk;
+                })
+                .on('end',() => {
+                    body=JSON.parse(body);
+                    console.log("data :", body);
+                });
+            }
+
+
+            else {
+                res.writeHead(404);
+            }
+        }   else {
+            res.writeHead(404);
+        }
+        
+        res.end();
+        // console.log(method, url); 
+        // res.end();
     })
-    .listen(port, () => {
+    // .createServer((request, response) => { //also a callback function
+    //     response.writeHead(200, {"Content-Type": "text/html"});
+    //     response.write("<h1>Hello, this is from my server</h1>");
+    //     response.end();
+    // })
+    .listen(port, () => { // callback function
         console.log(`Nodejs server started on port ${port}`);
     });
 
